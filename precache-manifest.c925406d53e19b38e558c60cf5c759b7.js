4de17df9adb24c809715e9b8cf4ce801151339e4
Ifham <ifhamhusain778866@gmail.com>
2023-01-22 19:01:51 +0530

    Updates

---
+++ precache-manifest.c925406d53e19b38e558c60cf5c759b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cf028276f11dbe2c5df",
+    "url": "/static/css/main.60677780.chunk.css"
+  },
+  {
+    "revision": "2cf028276f11dbe2c5df",
+    "url": "/static/js/main.2cf02827.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "9bffae4434ba33582fc2",
+    "url": "/static/js/2.9bffae44.chunk.js"
+  },
+  {
+    "revision": "b117a03f136dce80bebe",
+    "url": "/static/js/runtime~main.b117a03f.js"
+  },
+  {
+    "revision": "5427f5771543025deebd046740a358e2",
+    "url": "/index.html"
+  }
+];
